4de401013e92810681f6f99fc7272053b41d0445
ari <arifaiwu@gmail.com>
2022-04-21 13:59:15 -0300

    Finished excersice 1 - While

---
+++ Assets/Scripts/While01.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While01 : MonoBehaviour
+{
+    //Realizar un programa que muestre los numeros del 1 al 100 utilizando la instrucción while
+    int counter = 1;
+    int counterMax = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter <= counterMax)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+        Debug.Log("Terminó el while");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
